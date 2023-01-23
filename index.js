48b6f21c99fb063a28d7452d4c33333c2b7c1076
Leiladany <leiladany16@gmail.com>
2023-01-23 13:47:35 +0000

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ctx = myCanvas.getContext('2d')
 myCanvas.style.border = '8px solid white'
 
 const backImg = new Image()
-backImg.src = '../Escaping-Westview/Images/road.png'
+backImg.src = '../Escaping-Westview/Images/present.png'
 function animate() {
     ctx.drawImage(backImg, 0, 0, myCanvas.width, myCanvas.height)
 }
