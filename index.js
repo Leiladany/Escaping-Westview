3bc5f12a14e5648cae8bc5befcc1b1217b1a8370
Leiladany <leiladany16@gmail.com>
2023-01-24 00:35:22 +0000

    done

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ myCanvas.style.border = '8px solid white'
 const backImg = new Image()
 backImg.src = './Images/Background/present.png'
 
+const backImg2 = new Image()
+backImg2.src = './Images/Background/present.png'
+
 const playImg = new Image()
 playImg.src = './Images/Wanda/WandaPresent.png'
 
@@ -21,7 +24,10 @@ obsImg2.src = './Images/Fireballs/Purple.png'
 const playWidth = 250
 const playHeight = 200
 
-let playx = 0
+let backgroundy = 0
+let backgroundy2 = -myCanvas.width
+
+let playx = 800
 let playy = 0
 let playspeed = 5
 
@@ -31,20 +37,62 @@ let isMoveUp = false
 let isMoveDown = false
 let isNotMove = true
 
-let obsx = -10
+let obsx = 0
 let obsy = 0
 let obsspeed = 5
 
 let animateId
 let gameOver = false
 
+let obstacles =[]
+
+class Obstacle {
+  constructor(obsx, obsy, width, height) {
+    this.obsx = obsx
+    this.obsy = obsy
+    this.width = width
+    this.height = height
+} 
+
+draw() {
+  ctx.beginPath()
+  ctx.obsImg = new Image()
+  ctx.obsImg.src = './Images/Fireballs/Purple.png'
+  ctx.rect(this.obsx, this.obsy, this.width, this)
+  ctx.closePath
+}
+
+checkCollision() {
+
+    playx < this.obsx + this.width &&
+    playx + playWidth > this.obsx &&
+    playy < this.obsy + this.height &&
+     {
+    gameOver : true
+  }
+
+}
+}
+
 
 const startBtn = document.getElementById('start-button')
 
-const animate = () => {
-    ctx.drawImage(backImg, 0, 0, myCanvas.width, myCanvas.height)
+function animate () {
+    ctx.drawImage(backImg, backgroundy, 0, myCanvas.width, myCanvas.height)
+    ctx.drawImage(backImg2, backgroundy2, 0, myCanvas.width, myCanvas.height)
     ctx.drawImage(playImg, playx, playy, 200, 200)
-    ctx.drawImage(obsImg, obsx, 20, 20, 300)
+    ctx.drawImage(obsImg, obsx, 0, 200, 100)
+
+    backgroundy += 2
+    backgroundy2 += 2
+
+    if (backgroundy > myCanvas.width) {
+      backgroundy = -myCanvas.width
+    }
+
+    if (backgroundy2 > myCanvas.width) {
+      backgroundy2 = -myCanvas.width
+    }
 
     obsx += obsspeed
     obsy += obsspeed
@@ -53,7 +101,7 @@ const animate = () => {
         playx -= playspeed
       } else if (isMoveRight && playx < 1000) {playx += playspeed
       } else if (isNotMove) {
-        playx = 30 
+        playx = 1000
       }
 
       if (isMoveUp && playy > 0) {
@@ -63,8 +111,7 @@ const animate = () => {
         playy = 170
       }
       
-
-      if(!gameOver){
+      if (!gameOver){
         animateId = requestAnimationFrame(animate)
       } else {
       cancelAnimationFrame(animateId)
