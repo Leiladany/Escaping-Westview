562b1013e3abc0a0563c95691356698e5b21ebc3
Leiladany <leiladany16@gmail.com>
2023-01-25 17:45:10 +0000

    working on collision

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const myCanvas = document.querySelector('canvas')
 const ctx = myCanvas.getContext('2d')
 myCanvas.style.border = '8px solid #911717'
 
+
 const backImg = new Image()
-backImg.src = './Images/Background/present.png'
+backImg.src = './Images/Background/newback.jpg'
 
 const backImg2 = new Image()
 backImg2.src = './Images/Background/present.png'
@@ -32,7 +33,9 @@ let score = 0
 let animateId
 let intervalId = 0
 
-let spellMove = [{x: myCanvas.width, y: myCanvas.height -250}]
+let spellMove1 = [{x: myCanvas.width, y: myCanvas.height -230}]
+let spellMove2 = [{x: myCanvas.width, y: 200}]
+let spellMove3 = [{x: myCanvas.width, y: 300}]
 
 let startBtn = document.getElementById('start-button')
 let restartBtn = document.getElementById('restart')
@@ -54,20 +57,21 @@ function animate () {
       backgroundy2 = -myCanvas.width
     } */
 
-    for (let i=0; i<spellMove.length; i++) {
-      ctx.drawImage(spell, spellMove[i].x, spellMove[i].y, 120, 120)
+//1st spell
+    for (let i=0; i<spellMove1.length; i++) {
+      ctx.drawImage(spell, spellMove1[i].x, spellMove1[i].y, 120, 120)
 
-      spellMove[i].x = spellMove[i].x - spellSpeed
+      spellMove1[i].x = spellMove1[i].x - spellSpeed
     
-    if (spellMove[i].x <= 8 && spellMove[i].x > 0) {
+    if (spellMove1[i].x <= 8 && spellMove1[i].x > 0) {
       score++
     }
-    if (spellMove[i].x < -200) {
-      spellMove[i] = {x: 3000, y: 200}
+    if (spellMove1[i].x < -200) {
+      spellMove1[i] = {x: 3000, y: 200}
     }
 
-    let obst1 = {radius:40, x: playX, y: playY}
-    let obst2 = {radius:40, x: spellMove[i].x + 20, y: 200}
+    let obst1 = {radius:30, x: playX, y: playY}
+    let obst2 = {radius:40, x: spellMove1[i].x + 20, y: 200}
 
     let distx = obst1.x - obst2.x
     let disty = obst1.y - obst2.y
@@ -76,6 +80,56 @@ function animate () {
     if (distance < obst1.radius + obst2.radius) {
       gameOver = true
     }
+    }
+//2nd spell
+    for (let i=0; i<spellMove2.length; i++) {
+      ctx.drawImage(spell, spellMove2[i].x, spellMove2[i].y, 90, 90)
+
+      spellMove2[i].x = spellMove2[i].x - (spellSpeed + 2)
+    
+    if (spellMove2[i].x <= 8 && spellMove2[i].x > 0) {
+      score++
+    }
+    if (spellMove2[i].x < -200) {
+      spellMove2[i] = {x: 3000, y: 300}
+    }
+
+    let obst1 = {radius:40, x: playX, y: playY}
+    let obst2 = {radius:40, x: spellMove2[i].x + 20, y: 300}
+
+    let distx = obst1.x - obst2.x
+    let disty = obst1.y - obst2.y
+    let distance = Math.sqrt(distx * distx + disty * disty)
+
+    if (distance < obst1.radius + obst2.radius) {
+      gameOver = true
+    }
+  }
+//3rd spell
+for (let i=0; i<spellMove3.length; i++) {
+  ctx.drawImage(spell, spellMove3[i].x, spellMove3[i].y, 120, 120)
+
+  spellMove3[i].x = spellMove3[i].x - (spellSpeed + 3)
+
+if (spellMove3[i].x <= 8 && spellMove3[i].x > 0) {
+  score++
+}
+if (spellMove3[i].x < -200) {
+  spellMove3[i] = {x: 3000, y: 400}
+}
+
+let obst1 = {radius:40, x: playX, y: playY}
+let obst2 = {radius:40, x: spellMove3[i].x + 20, y: 400}
+
+let distx = obst1.x - obst2.x
+let disty = obst1.y - obst2.y
+let distance = Math.sqrt(distx * distx + disty * disty)
+
+if (distance < obst1.radius + obst2.radius) {
+  gameOver = true
+}
+}
+
 
     if (isMoveLeft && playX > -10) {
         playX -= playSpeed
@@ -130,7 +184,6 @@ function animate () {
         })
     
 }
-}
 
 function lose() {
   gameOver = false;
@@ -141,7 +194,9 @@ function lose() {
 
   playX= 70
   playY= 200
-  spellMove = [{x: myCanvas.width , y: 200}]
+  spellMove1 = [{x: myCanvas.width, y: myCanvas.height -230}]
+  spellMove2 = [{x: myCanvas.width, y: 200}]
+  spellMove3 = [{x: myCanvas.width, y: 300}]
   let Score = document.querySelector(".gameOver h2")
 Score.innerHTML = `Score: ${score}`
 }
